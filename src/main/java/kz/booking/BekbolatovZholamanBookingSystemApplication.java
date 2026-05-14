3b8d67a0f2625fa7a1cb7353f0cf12649582ab45
zen_vov <122712538+zen-vov@users.noreply.github.com>
2026-05-14 21:07:00 +0500

    chore: init spring boot booking-system skeleton

---
+++ src/main/java/kz/booking/BekbolatovZholamanBookingSystemApplication.java
@@ -0,0 +1,13 @@
+package kz.booking;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableAsync;
+
+@EnableAsync
+@SpringBootApplication
+public class BekbolatovZholamanBookingSystemApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(BekbolatovZholamanBookingSystemApplication.class, args);
+    }
+}
